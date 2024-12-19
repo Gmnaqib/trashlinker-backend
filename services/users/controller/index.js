9c856070c892e30ab27220dd45eb8c3c88f73065
Gumilarmn <gumilarmn@gmail.com>
2024-12-19 11:24:20 +0700

    refactor error logging in addUser function

--- services/users/controller/index.js
+++ services/users/controller/index.js
@@ -1,4 +1,4 @@
-const UserRepository = require('../repository'); // Pastikan file ini benar
+const UserRepository = require('../repository'); 
 const userRepo = new UserRepository();
 
 module.exports = {
@@ -9,7 +9,7 @@ module.exports = {
         const newUser = await userRepo.addUserRepository(email, username, password, address, role);
         return res.json({User: req.body});
     } catch (error) {
-        console.error("Error in addUser:", error); // Log error di server
+        console.error("Error in addUser:", error); 
         return res.status(500).json({ error: "Terjadi kesalahan pada server" });
     }
   },
