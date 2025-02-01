944e4a4171da989c39d0520e345dd2fa8d730a6d
Gumilarmn <gumilarmn@gmail.com>
2025-02-01 21:33:32 +0700

    feat: add CORS support and implement post deletion functionality

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const router = require('./router/index')
+const express = require('express');
+const app = express();
+const port = 3000;
+const router = require('./router/index');
+const cors = require('cors');
 require('dotenv').config();
 
-app.use(express.json());
-app.use('/', router)
+// Mengizinkan semua origin dengan CORS
+app.use(cors());
 
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
-  console.warn(`Listening on http://localhost:${port}`)
-})
+  console.warn(`Listening on http://localhost:${port}`);
+});
