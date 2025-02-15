e8d777a24245b6bcd46ed45136d25be5a700fad3
Gumilarmn <gumilarmn@gmail.com>
2025-02-15 09:53:46 +0700

    Enhance file naming in upload storage to include time and milliseconds

--- utils/upload.js
+++ utils/upload.js
@@ -1,7 +1,6 @@
 const multer = require("multer");
 const path = require("path");
 
-// Konfigurasi penyimpanan
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/image");
@@ -11,13 +10,19 @@ const storage = multer.diskStorage({
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, "0");
     const day = String(now.getDate()).padStart(2, "0");
-    const formattedDate = `post-${year}-${month}-${day}-post${path.extname(
-      file.originalname
-    )}`;
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
+
+    const formattedDate = `post-${year}-${month}-${day}-${hours}${minutes}${seconds}${milliseconds}${path.extname(file.originalname)}`;
 
     cb(null, formattedDate);
   },
 });
+
+module.exports = storage;
+
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
